21ddaf4943def5e0573287330f253f55f19083ce
ArchILLtect <nickhansonsr@gmail.com>
2025-11-11 07:20:15 -0600

    refactor: add the test methods for schedule

--- src/test/java/com/foodtruck/persistence/ScheduleDaoTest.java
+++ src/test/java/com/foodtruck/persistence/ScheduleDaoTest.java
@@ -5,6 +5,9 @@ import com.foodtruck.entities.Location;
 import com.foodtruck.entities.Schedule;
 import com.foodtruck.entities.Truck;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class ScheduleDaoTest {
 
@@ -14,7 +17,6 @@ public class ScheduleDaoTest {
 
     @Before
     public void setup() {
-
         // Reset DB
         Database database = Database.getInstance();
         database.runSQL("cleanDB.sql");
@@ -25,4 +27,20 @@ public class ScheduleDaoTest {
         locationDao = new GenericDao<>(Location.class);
     }
 
+    @Test
+    public void getByIdSuccess() {
+        Schedule schedule = scheduleDao.getById(2);
+        assertNotNull(schedule);
+        assertEquals("Tuesday", schedule.getDayOfWeek());
+        assertEquals("5/8/2026", schedule.getDate());
+        assertEquals("8:00am", schedule.getStartTime());
+        assertEquals("4:00pm", schedule.getEndTime());
+        assertEquals(1, schedule.getTruck().getId());
+        assertEquals(1, schedule.getLocation().getLocationId());
+    }
+
+    @Test
+    public void getByIdMissingReturnsNull() {
+        assertNull(scheduleDao.getById(999999));
+    }
 }
